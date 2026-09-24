dcaeaf5646e68b27e6f75ed4e84b7c005a58eaba
arranjan516-boop <arranjan516@gmail.com>
2026-09-24 20:01:49 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,19 @@
+<?php
+// Database configuration
 
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "food_waste_donation";
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database Connection Failed: " . $conn->connect_error);
+}
+
+// Set UTF-8 character set
+$conn->set_charset("utf8mb4");
+?>
