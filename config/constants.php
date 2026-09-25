ece1f8387c97f39b8f720bc6c3eccf255af25707
arranjan516-boop <arranjan516@gmail.com>
2026-09-25 20:09:11 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,1 +1,24 @@
+<?php
 
+define("SITE_NAME", "Food Waste Donation System");
+
+define("BASE_URL", "/food-waste-donation-main/");
+
+define("MAX_RADIUS_KM", 15);
+
+define(
+    "UPLOAD_FOOD",
+    __DIR__ . "/../uploads/food/"
+);
+
+define(
+    "UPLOAD_PROFILE",
+    __DIR__ . "/../uploads/profiles/"
+);
+
+define(
+    "UPLOAD_NGO",
+    __DIR__ . "/../uploads/ngo/"
+);
+
+?>
