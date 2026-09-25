0074a151ef0ff0e709920b9898a7f7747dc06fff
arranjan516-boop <arranjan516@gmail.com>
2026-09-25 20:12:11 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,27 @@
+</main>
 
+
+<footer>
+
+    <div class="footer-content">
+
+        <h3>
+            Food Waste Donation System
+        </h3>
+
+        <p>
+            Reduce food waste. Share food. Help people.
+        </p>
+
+        <p>
+            © 2026 Food Waste Donation System
+        </p>
+
+    </div>
+
+</footer>
+
+
+</body>
+
+</html>
