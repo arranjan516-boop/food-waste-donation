7a5f7b96b92b381407b838673ce2a1608da7829e
arranjan516-boop <arranjan516@gmail.com>
2026-09-25 20:11:04 +0530

    Update role-check.php

--- includes/role-check.php
+++ includes/role-check.php
@@ -1,1 +1,25 @@
+<?php
 
+require_once __DIR__ . "/auth.php";
+
+
+function require_role($required_role)
+{
+    require_login();
+
+    if (
+        !isset($_SESSION["role"]) ||
+        $_SESSION["role"] !== $required_role
+    ) {
+
+        header(
+            "Location: " .
+            BASE_URL .
+            "index.php"
+        );
+
+        exit;
+    }
+}
+
+?>
