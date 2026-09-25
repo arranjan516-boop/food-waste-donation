48293532f9171df2cd14775240f8348055f85c04
arranjan516-boop <arranjan516@gmail.com>
2026-09-25 20:10:34 +0530

    Update auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,1 +1,22 @@
+<?php
 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+function require_login()
+{
+    if (empty($_SESSION["user_id"])) {
+
+        header(
+            "Location: " .
+            BASE_URL .
+            "login.php"
+        );
+
+        exit;
+    }
+}
+
+?>
